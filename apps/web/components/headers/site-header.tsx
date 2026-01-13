1356462667948fbc31637d65730cbe266a626ea7
zersix225 <drago.overlond@gmail.com>
2026-01-13 14:56:15 +0700

    chore: fix link nevigate at sidebar

--- apps/web/components/headers/site-header.tsx
+++ apps/web/components/headers/site-header.tsx
@@ -14,8 +14,14 @@ import {
 
 export function SiteHeader() {
   const pathname = usePathname();
-  const format = pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2);
 
+  const pathFormat = pathname
+    .split("/")
+    .slice(1)
+    .map((p) => {
+      const path = p.slice(1);
+      return p.charAt(0).toUpperCase() + path;
+    });
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -27,12 +33,16 @@ export function SiteHeader() {
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">{format}</BreadcrumbLink>
+              <BreadcrumbLink href="/form">{pathFormat[0]}</BreadcrumbLink>
             </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
+            {pathFormat.length === 2 && (
+              <BreadcrumbList>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{pathFormat[1]}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            )}
           </BreadcrumbList>
         </Breadcrumb>
       </div>
--- apps/web/constants/nav.js
+++ apps/web/constants/nav.js
@@ -1,40 +1,18 @@
-"use client";
-
-import * as React from "react";
 import {
   IconCamera,
-  IconChartBar,
   IconDashboard,
   IconDatabase,
   IconFileAi,
   IconFileDescription,
   IconFileWord,
-  IconFolder,
   IconHelp,
-  IconInnerShadowTop,
   IconListDetails,
   IconReport,
   IconSearch,
   IconSettings,
-  IconUsers,
 } from "@tabler/icons-react";
 
-import NavDocuments from "@/components/nav/nav-documents";
-import NavMain from "@/components/nav/nav-main";
-import NavSecondary from "@/components/nav/nav-secondary";
-import NavUser from "@/components/nav/nav-user";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@repo/shadcn/components/sidebar";
-import { nav } from "@/constants/nav";
-
-const data = {
+export const nav = {
   user: {
     name: "shadcn",
     email: "m@example.com",
@@ -142,35 +120,3 @@ const data = {
     },
   ],
 };
-
-export default function AppSidebar({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
-              <a href="#">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Somboon Inc.</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={nav.navMain} />
-        <NavDocuments items={nav.documents} />
-        {/*<NavSecondary items={data.navSecondary} className="mt-auto" />*/}
-      </SidebarContent>
-      {/*<SidebarFooter>*/}
-      {/*  <NavUser user={data.user} />*/}
-      {/*</SidebarFooter>*/}
-    </Sidebar>
-  );
-}
