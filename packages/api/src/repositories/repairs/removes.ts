a928b937ecfae5d83f3bc4429e9cb76e7923dc1b
zersix225 <drago.overlond@gmail.com>
2026-03-10 14:36:18 +0700

    fix(ci): fixed error from workflow

--- packages/api/src/repositories/repairs/removes.ts
+++ packages/api/src/repositories/repairs/removes.ts
@@ -1,6 +1,7 @@
 import type { PrismaType } from "@/config/prisma";
 import type { RepairRepository } from "@/types/repositories/repair";
 import { RepairWithRelationsSchema, Helpers } from "@/schemas";
+import { Prisma } from "@prisma/client/extension";
 
 export function remove(prismaClient: PrismaType): RepairRepository["remove"] {
   return async (id) => {
@@ -8,19 +9,21 @@ export function remove(prismaClient: PrismaType): RepairRepository["remove"] {
       where: { id },
     });
     if (!existRepair) return null;
-    const result = await prismaClient.$transaction(async (tx) => {
-      await tx.service.deleteMany({
-        where: { repair_id: id },
-      });
+    const result = await prismaClient.$transaction(
+      async (tx: Prisma.TransactionClient) => {
+        await tx.service.deleteMany({
+          where: { repair_id: id },
+        });
 
-      return tx.repair.delete({
-        where: { id },
-        include: {
-          service: true,
-          customer: true,
-        },
-      });
-    });
+        return tx.repair.delete({
+          where: { id },
+          include: {
+            service: true,
+            customer: true,
+          },
+        });
+      },
+    );
     return Helpers.fromObjectToSchema(RepairWithRelationsSchema.Schema)(result);
   };
 }
